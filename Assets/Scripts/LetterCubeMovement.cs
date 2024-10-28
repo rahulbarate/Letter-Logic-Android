a0e084b8ca393feb1f82ae35783d52f515abced7
Rahul Barate <baraterahul48@gmail.com>
2024-10-28 18:05:08 +0530

    Completed LLPW-6,Created Letter Cube and wrote script for it's movement

---
+++ Assets/Scripts/LetterCubeMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LetterCubeMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // disabling cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovement();
+
+        //enabling cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void ProcessMovement()
+    {
+        float xVal = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        float zVal = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Translate(xVal, 0f, zVal);
+    }
+}
