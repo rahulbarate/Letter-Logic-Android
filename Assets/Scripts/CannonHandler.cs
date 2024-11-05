d7099ce7e1b246d489650b6203fc31c5dd8ad31d
Rahul Barate <baraterahul48@gmail.com>
2024-11-05 18:22:23 +0530

    Added cannon, wrote script to instantiate and throw cannon ball, added script to handle cannon ball

---
+++ Assets/Scripts/CannonHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonHandler : MonoBehaviour
+{
+
+    [SerializeField] GameObject cannonBallCopy;
+    [SerializeField] GameObject cannonBallSpawnPoint;
+    [SerializeField] float forceAmount = 500f;
+    GameObject instantiatedCannonBall;
+    Rigidbody cannonBallRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FireCannon();
+        }
+    }
+    public void FireCannon()
+    {
+        instantiatedCannonBall = Instantiate(cannonBallCopy, cannonBallSpawnPoint.transform.position, Quaternion.identity);
+        instantiatedCannonBall.SetActive(true);
+        instantiatedCannonBall.transform.localScale = new Vector3(27f, 27f, 27f);
+        instantiatedCannonBall.AddComponent<Rigidbody>();
+        cannonBallRigidbody = instantiatedCannonBall.GetComponent<Rigidbody>();
+        Vector3 localForce = new Vector3(0f, 0f, forceAmount);
+        Vector3 worldForce = transform.TransformDirection(localForce);
+        cannonBallRigidbody.AddForce(worldForce, ForceMode.Impulse);
+    }
+}
